957ff15a069ea3ab6336204406974ed8cc68a3ec
Emilio Jesús Hernández Salas <emiliohersal@uma.es>
2026-01-11 20:20:16 +0100

    ooootro cambio

--- js/guard.js
+++ js/guard.js
@@ -1,4 +1,9 @@
 async function checkAuth() {
+  if (!window.supabaseClient) {
+    console.error("Supabase no inicializado")
+    return window.location.href = "index.html"
+  }
+
   const { data: { session } } = await window.supabaseClient.auth.getSession()
 
   if (!session) {
--- js/logout.js
+++ js/logout.js
@@ -1,4 +1,8 @@
 async function logout() {
+  if (!window.supabaseClient) {
+    console.error("Supabase no inicializado")
+    return
+  }
   await window.supabaseClient.auth.signOut()
   window.location.href = "index.html"
 }
